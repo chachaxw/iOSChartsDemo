9c8a4c3b52c65249e3ea0fe18bd8583b6c88d22d
Wei Zhou <867571123@qq.com>
2016-12-24 23:48:47 +0800

    Revert "添加 chart 框架"
    This reverts commit dc330d22c8f865f08c090c246d8c1be21d4630ee.

--- iOSChartsDemo/BarChartViewController.swift
+++ iOSChartsDemo/BarChartViewController.swift
@@ -7,39 +7,13 @@
 //
 
 import UIKit
-import Charts
 
 class BarChartViewController: UIViewController {
 
-    @IBOutlet weak var barChartView: BarChartView!
-    
-    var months: [String]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
-        setChart(dataPoints: months, values: unitsSold)
-    
     }
-    
-    func setChart(dataPoints: [String], values: [Double]) {
-        barChartView.noDataText = "You need to provide data for the chart."
-        
-        var dataEntries: [BarChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(x: values[i], y: Double(i))
-            dataEntries.append(dataEntry)
-        }
-        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
-        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
-        barChartView.data = chartData
-    }
-    
-    
 
 
 }
