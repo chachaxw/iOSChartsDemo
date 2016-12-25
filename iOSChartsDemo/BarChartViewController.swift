ca3e1f1cb74fb9dad374e71e6216fa49329743d3
Wei Zhou <867571123@qq.com>
2016-12-25 18:04:02 +0800

    添加条形图展示示例

--- iOSChartsDemo/BarChartViewController.swift
+++ iOSChartsDemo/BarChartViewController.swift
@@ -11,11 +11,62 @@ import Charts
 
 class BarChartViewController: UIViewController {
 
+    @IBOutlet weak var barView: BarChartView!
+    
+    weak var axisFormatDelegate: IAxisValueFormatter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        barView.noDataText = "There's no data in your chart"
+        
+        updateChartWithData()
     }
 
+    func updateChartWithData() {
+        var dataEntries: [BarChartDataEntry] = []
+//        let visitorCounts = getVisitorCountsFromDataBase()
+        let visitorCounts = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        for i in 0..<visitorCounts.count {
+//            let timeIntervalForDate: TimeInterval = visitorCounts[i].date.timeIntervalSince1970
+            let dataEntry = BarChartDataEntry(x: Double(visitorCounts[i]), y: Double(i))
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Visitor Count")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barView.data = chartData
+        
+        let xaxis = barView.xAxis
+        xaxis.valueFormatter = axisFormatDelegate
+        
+    }
+    
+    func getVisitorCountsFromDataBase() {
+        
+    }
 
 }
+
+// MARK: axisFormatDelegate
+//extension ViewController: IAxisValueFormatter {
+//    
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ iOSChartsDemo/VisitorCount.swift
@@ -0,0 +1,28 @@
+//
+//  VisitorCount.swift
+//  iOSChartsDemo
+//
+//  Created by Wei Zhou on 25/12/2016.
+//  Copyright © 2016 Appcoda. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class VisitorCount: Object {
+    dynamic var date: Date = Date()
+    dynamic var count: Int = Int(0)
+    
+    func save() {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(self)
+            }
+        } catch let error as NSError {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    
+}
